535642d9887bd4216e55e97685144766ab8d9266
theMorshed <themorshedctg@gmail.com>
2025-01-06 21:13:34 +0600

    Main layout refactor

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,43 +1,60 @@
-import { Layout, Menu } from "antd";
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { createElement } from "react";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-        key: String(index + 1),
-        icon: createElement(icon),
-        label: `nav ${index + 1}`,
-    }),
-);
+const items: MenuProps['items'] = [
+    {
+        key: 1,
+        label: 'Dashboard'
+    },
+    {
+        key: 2,
+        label: 'Profile'
+    },
+    {
+        key: 3,
+        label: 'Users',
+        children: [
+            {
+                key: 31,
+                label: 'Create admin'
+            },
+            {
+                key: 32,
+                label: 'Create faculty'
+            }
+        ]
+    }
+]
 
 const MainLayout = () => {
     return (
-        <Layout>
+        <Layout style={{height: '100vh'}}>
             <Sider
                 breakpoint="lg"
                 collapsedWidth="0"
                 onBreakpoint={(broken) => {
-                console.log(broken);
+                    console.log(broken);
                 }}
                 onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
+                    console.log(collapsed, type);
                 }}
             >
-                <div className="demo-logo-vertical" />
+                <div style={{color: "white", display: "flex", alignItems: "center", justifyContent: "center", marginTop: "20px", marginBottom: "16px"}}>
+                    <h1>PH UNIVERSITY</h1>
+                </div>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
             </Sider>
             <Layout>
                 <Header style={{ padding: 0}} />
                 <Content style={{ margin: '24px 16px 0' }}>
-                <div
-                    style={{
-                        padding: 24,
-                        minHeight: 360,
-                    }}
-                >
-                    Hello world, Welcome back with ant design
-                </div>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                        }}
+                    >
+                        Hello world, Welcome back with ant design
+                    </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>
                     Ant Design ©{new Date().getFullYear()} Created by Ant UED
