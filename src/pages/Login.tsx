73106f9ec70f6aef12d4bb01a2b336ae44b3b603
theMorshed <themorshedctg@gmail.com>
2025-01-13 15:26:46 +0600

    setup login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,25 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+    const { handleSubmit, register } = useForm();
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            This is login page.
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID: </label>
+                <input id="id" type="text" {...register('id')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id="password" type="text" {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
