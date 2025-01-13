41036c4fa9dc7b122f1fc0ef09d8d09627a50b64
theMorshed <themorshedctg@gmail.com>
2025-01-13 21:09:59 +0600

    add redux persist and solve some problem also add protected route

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice'
 import { baseApi } from "./api/baseApi";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
-        auth: authReducer
+        auth: persistedReducer
     },
-    middleware: getDefaultMiddleWares => getDefaultMiddleWares().concat(baseApi.middleware)
+    middleware: getDefaultMiddleWares => getDefaultMiddleWares({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    }).concat(baseApi.middleware)
 })
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store);
