cc8d4b2c34589aad94a9cbfd9caf32b4786f7a55
theMorshed <themorshedctg@gmail.com>
2025-01-15 14:27:39 +0600

    typescript error type created in global types

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -9,11 +9,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { academicSemesterSchema } from "../../../schemas/academicSemester.schema";
 import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagementApi";
 import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
 
 const CreateAcademicSemester = () => {
     const [addAcademicSemester] = useAddAcademicSemesterMutation();
 
     const onSubmit:SubmitHandler<FieldValues> = async(data) => {
+        const toastId = toast.loading('Creating semester...');
         const name = semesterOptions[Number(data.name) - 1]?.label;
         const semesterData = {
             name: name,
@@ -22,12 +24,15 @@ const CreateAcademicSemester = () => {
             startMonth: data.startMonth,
             endMonth: data.endMonth
         }
-        try {
-            console.log(semesterData);
-            const res = await addAcademicSemester(semesterData);
-            console.log(res);
+        try {            
+            const res = await addAcademicSemester(semesterData) as TResponse;
+            if (res.error) {
+                toast.error(res.error.data.message, {id: toastId});
+            } else {
+                toast.success('Semester created successfully', {id: toastId});
+            }
         } catch(err) {
-            toast.error('Something went wrong');
+            toast.error('Something went wrong', {id: toastId});
         }
     }
 
