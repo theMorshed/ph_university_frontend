8d747c6d3e40f5717eeaad4c6a2d64cf5ae1f723
theMorshed <themorshedctg@gmail.com>
2025-01-18 21:33:18 +0600

    create userManagement directory, userManagement api and create student component with a form

--- src/pages/admin/CreateStudent.tsx
+++
@@ -1,9 +0,0 @@
-const CreateStudent = () => {
-    return (
-        <div>
-            Student creation form will be here.
-        </div>
-    );
-};
-
-export default CreateStudent;
---
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -0,0 +1,21 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { Button } from "antd";
+
+const CreateStudent = () => {
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
+        const formData = new FormData();
+        formData.append('data', JSON.stringify(data));
+        console.log(Object.fromEntries(formData));
+    }
+    return (
+        <PHForm onSubmit={onSubmit}>
+            <PHInput type="text" name="name" label="Name" />
+            <Button htmlType="submit">submit</Button>
+        </PHForm>
+    );
+};
+
+export default CreateStudent;
